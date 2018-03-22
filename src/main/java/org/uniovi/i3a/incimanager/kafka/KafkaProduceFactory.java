82c3d913fd3af1f59e1ece8dcdffa4b2b0ee4288
Víctor Suárez Fernández <vicsufer@gmail.com>
2018-03-22 17:50:48 +0100

    IKafkaService defined

--- src/main/java/org/uniovi/i3a/incimanager/kafka/KafkaProduceFactory.java
+++ src/main/java/org/uniovi/i3a/incimanager/kafka/KafkaProduceFactory.java
@@ -15,8 +15,6 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
-@Configuration
-@EnableKafka
 public class KafkaProduceFactory {
 
 	@Value("${spring.kafka.producer.bootstrap-servers}")
